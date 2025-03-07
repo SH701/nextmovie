08f4b329dfb853235ed19c8fa1ee89d8736be69e
SH701 <125341405+SH701@users.noreply.github.com>
2025-03-07 14:17:22 +0900

    Fix:Type error in prams

--- app/(movie)/movies/[id]/page.tsx
+++ app/(movie)/movies/[id]/page.tsx
@@ -2,28 +2,29 @@ import { Suspense } from "react";
 import MovieInfo, { getMovie } from "../../../components/movie-info";
 import MovieVideos from "../../../components/movie-videos";
 
-interface IParams{
-  params:{id:string};
-}
+type IParams = Promise<{
+  id: string;
+}>;
 
-export async function generateMetadata({params:{id},}: IParams){
-  const movie = await getMovie(id);
+export async function generateMetadata(props: { params: IParams }) {
+  const params = await props.params;
+  const id = params.id;
   return {
-    title:movie.title,
-  }
+    title: id,
+  };
 }
 
-export default async function MovieDetailPage({
-    params: { id },
-  }: IParams) {
-    return (
-      <div>
-        <Suspense fallback={<h1>Loading movie info</h1>}>
-          <MovieInfo id={id} />
-        </Suspense>
-        <Suspense fallback={<h1>Loading movie videos</h1>}>
-          <MovieVideos id={id} />
-        </Suspense>
-      </div>
-    );
-  }
+
+export default async function MovieDetailPage(props:{ params : IParams}) {
+  const params = await props.params;
+  return (
+    <div>
+      <Suspense fallback={<h1>Loading movie info</h1>}>
+        <MovieInfo id={params.id} />
+      </Suspense>
+      <Suspense fallback={<h1>Loading movie videos</h1>}>
+        <MovieVideos id={params.id} />
+      </Suspense>
+    </div>
+  );
+}
