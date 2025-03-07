393d55166a083416ab3363bafed552c665d97e3c
SH701 <125341405+SH701@users.noreply.github.com>
2025-03-07 13:54:37 +0900

    1차변경

--- app/components/movie-info.tsx
+++ app/components/movie-info.tsx
@@ -1,6 +1,7 @@
 import { API_url } from "../(home)/page";
+import styles from "../../styles/movie-info.module.css"
 
-async function getMovie(id:string) {
+export async function getMovie(id:string) {
     const response  = await fetch(`${API_url}/${id}`,{});
     return response.json();
 
@@ -8,5 +9,16 @@ async function getMovie(id:string) {
 
 export default async function MovieInfo({id}:{id:string}) {
     const movie = await getMovie(id);
-    return <h6>{JSON.stringify(movie)}</h6>;
+    return (
+        <div className={styles.container}> 
+            <img className={styles.poster} src={movie.backdrop_path}/>
+            <div>
+                <h1 className={styles.title}>{movie.title}</h1>
+                <h3>⭐{movie.vote_average.toFixed(1)}</h3>
+                <p className={styles.info}>{movie.overview}</p>
+                <a href={movie.homepage}
+                target={"_blank"}>homepage &rarr;</a>
+            </div>
+        </div>
+    )
 }
