9a4d57e572e8a39a189a2bcd8a3578ad925ccf28
SH701 <125341405+SH701@users.noreply.github.com>
2025-03-11 13:27:20 +0900

    credit add

--- app/(movie)/movies/[id]/similar/page.tsx
+++ app/(movie)/movies/[id]/similar/page.tsx
@@ -2,18 +2,22 @@ import SimilarMovie from "../../../../components/similarmovie";
 import { API_url } from "../../../../config";
 import styles from "../similar/similar.module.css";
 
+interface SimilarPageProps {
+    params: { id: string };
+  }
+
 async function getSimilarMovie(id: string) {
     const response = await fetch(`${API_url}/${id}/similar`);
     return response.json();
 }
 
 
-export default async function Similar({ params }: { params: { id: string } }) {
+export default async function Similar({ params }: SimilarPageProps) {
     const similars = await getSimilarMovie(params.id);
 
     return (
         <div className={styles.container}>
-            {similars.results.map((similar) => ( // ✅ API 응답 구조에 맞게 `results` 추가
+            {similars.map((similar) => ( 
                 <SimilarMovie 
                     key={similar.id}
                     id={similar.id} 
