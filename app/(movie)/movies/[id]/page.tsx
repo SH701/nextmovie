e2cdd8da4848c327492d1df5ed137793bde7ba7e
SH701 <125341405+SH701@users.noreply.github.com>
2025-03-11 14:52:56 +0900

    info styl

--- app/(movie)/movies/[id]/page.tsx
+++ app/(movie)/movies/[id]/page.tsx
@@ -3,26 +3,28 @@ import MovieInfo, { getMovie } from "../../../components/movie-info";
 import MovieVideos from "../../../components/movie-videos";
 
 
-interface IParams {
-  params :{id: string};
+interface PageProps  {
+  params :{
+    id: string
+  };
 }
 
-export async function generateMetadata({ params: { id } }: IParams) {
-  const movie = await getMovie(id);
+export async function generateMetadata({ params}: PageProps ) {
+  const movie = await getMovie(params.id);
   return {
     title: movie.title,
   };
 }
 
 
-export default async function MovieDetailPage({ params: { id } }: IParams) {
+export default async function MovieDetailPage({ params }: PageProps ) {
   return (
     <div>
       <Suspense fallback={<h1>Loading movie info</h1>}>
-        <MovieInfo id={id} />
+        <MovieInfo id={params.id} />
       </Suspense>
       <Suspense fallback={<h1>Loading movie videos</h1>}>
-        <MovieVideos id={id} />
+        <MovieVideos id={params.id} />
       </Suspense>
     </div>
   );
