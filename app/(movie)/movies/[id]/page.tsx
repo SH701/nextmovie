9c69831bc6a31c522fef85d6d85a6450e9708eb7
SH701 <125341405+SH701@users.noreply.github.com>
2025-03-11 15:10:39 +0900

    params

--- app/(movie)/movies/[id]/page.tsx
+++ app/(movie)/movies/[id]/page.tsx
@@ -2,27 +2,28 @@ import { Suspense } from "react";
 import MovieInfo, { getMovie } from "../../../components/movie-info";
 import MovieVideos from "../../../components/movie-videos";
 
-interface PageProps {
-  query: {
-    id: string;
-  };
-}
 
-export async function generateMetadata({ query }: PageProps) {
-  const movie = await getMovie(query.id);
+type IParams = Promise<{
+  id: string;
+}>;
+
+export async function generateMetadata({ params }: { params: { id: string } }) {
+  const movie = await getMovie(params.id);
   return {
     title: movie.title,
   };
 }
 
-export default async function MovieDetailPage({ query }: PageProps) {
+
+export default async function MovieDetailPage(props:{ params : IParams}) {
+  const params = await props.params;
   return (
     <div>
       <Suspense fallback={<h1>Loading movie info</h1>}>
-        <MovieInfo id={query.id} />
+        <MovieInfo id={params.id} />
       </Suspense>
       <Suspense fallback={<h1>Loading movie videos</h1>}>
-        <MovieVideos id={query.id} />
+        <MovieVideos id={params.id} />
       </Suspense>
     </div>
   );
